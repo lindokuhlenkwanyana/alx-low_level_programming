654c7359f1bb8e2216b5291dba90e0c5ee60210d
Lindokuhle Nkwanyana <brandynkwanyana0409@gmail.com>
2023-06-15 12:08:08 -0700

    A c program

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,2 +1,12 @@
-#!/bin/bash 
-gcc -S -masm-intel $CFILE
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - A program that outputs lines to the default error
+ * Return: 1
+ */
+int main(void)
+{
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n, 59);
+return (1);
+}
