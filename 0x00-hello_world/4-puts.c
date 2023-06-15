21ea1a5869d94e56eb5d476c0ae835a9c5b2e593
Lindokuhle Nkwanyana <brandynkwanyana0409@gmail.com>
2023-06-15 07:40:50 -0700

    My first c program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main : A program having a push function that outputs a line
+ * main - A program that uses a put function to print a line
  * Return: 0
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle);
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
