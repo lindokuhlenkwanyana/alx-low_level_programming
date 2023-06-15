b025fa09f7f348b3db5cef5644d781ab5b3a0956
Lindokuhle Nkwanyana <brandynkwanyana0409@gmail.com>
2023-06-15 12:16:58 -0700

    inserted a missing colon

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <unistd.h>
-
+#include <unistd.h>
 /**
  * main - A program that outputs lines to the default error
  * Return: 1
  */
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n, 59);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
