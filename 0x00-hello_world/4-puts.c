9bc228d14a41d45fe3300683fa527317fe779e0e
Lindokuhle Nkwanyana <brandynkwanyana0409@gmail.com>
2023-06-15 06:34:22 -0700

    My hello world first programm

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main: A programm having a push function that outputs a line
+ * Return: 0
+ */
+int main(void)
+{
+printf("\"with proper grammar, but the outcome is a piece of art");
+return (0);
+}
