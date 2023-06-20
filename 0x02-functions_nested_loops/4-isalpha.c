4516dc1ae3e049f50202ce3bdb9db6b300675828
Lindokuhle Nkwanyana <brandynkwanyana0409@gmail.com>
2023-06-20 09:50:32 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,7 +2,6 @@
 
 
 /**
-
  * _isalpha - checks for alphabetic character
  * @c: the character to be checked
  * Return: 1 if c is a letter, 0 otherwise
@@ -12,6 +11,6 @@ int _isalpha(int c)
 
 {
 
-        return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 
 }
