7e5d6e7f3a34b198a9b017e13aa1af8f1f2adb9c
Lindokuhle Nkwanyana <brandynkwanyana0409@gmail.com>
2023-06-20 11:22:33 -0700

    fixed trailing white space

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -20,12 +20,13 @@ _putchar(',');
 _putchar(' ');
 _putchar(' ');
 _putchar(k + '0');
-} else if (k >= 10)
+}
+else if (k >= 10)
 {
 _putchar(',');
 _putchar(' ');
 _putchar((k / 10) + '0');
-_putchar((k % 10) + '0');                       
+_putchar((k % 10) + '0');
 }
 }
 _putchar('\n');
