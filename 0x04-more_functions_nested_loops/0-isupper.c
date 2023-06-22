4f5f77cb970375f53bed1f1becfec003a2e63465
Lindokuhle Nkwanyana <brandynkwanyana0409@gmail.com>
2023-06-22 09:36:23 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include <main.h>
+
+/**
+ * _isupper - checking if a letter is upper
+ * @x: the number to be checked
+ * Return: 1
+ */
+
+int _isupper(int x)
+{
+if (x >= 65 && x <= 90)
+{
+return (1);
+}
+return (0);
+}
