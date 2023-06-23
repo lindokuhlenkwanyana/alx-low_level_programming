dd5b05bdf81d4a9cc010b542def1407b3040b20d
Lindokuhle Nkwanyana <brandynkwanyana0409@gmail.com>
2023-06-23 04:52:32 -0700

    printing a diagonal line

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,14 +7,14 @@
 
 void print_diagonal(int n)
 {
-int lIne, diaGonal;
+int lIne, diaGonaln;
 if (n <= 0)
 _putchar('\n');
 else
 {
 for (lIne = 1; lIne <= n; lIne++)
 {
-for (diaGonal = 1; diaGonal <= lIne; diaGonal++)
+for (diaGonaln = 1; diaGonaln <= lIne; diaGonaln++)
 _putchar(' ');
 _putchar(92); /* is equal to '/' char*/
 _putchar('\n');
