903185fc7741cfd50ee98e3f7312e82af756f82e
Lindokuhle Nkwanyana <brandynkwanyana0409@gmail.com>
2023-06-23 10:59:59 -0700

    a square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -8,12 +8,22 @@
 
 void print_square(int size)
 
+
 {
-int row, column;
-for (row = 1; row <= size; row++)
+int x, y;
+if (size <= 0)
 {
-for (column = 1; column <= size; column++)
-_putchar('#');
 _putchar('\n');
+}
+else
+{
+for (x = 0; x < size; x++)
+{
+for (y = 0; y < size; y++)
+{
+_putchar(35);
+}
+_putchar('\n');
+}
 }
 }
