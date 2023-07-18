b1bc59f08f24127d0bf57294ec3fffde0dfdcc68
Lindokuhle Nkwanyana <brandynkwanyana0409@gmail.com>
2023-07-18 04:00:33 -0700

    A function that creates a new dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include "dog.h"
+#include <stdio.h>
+ 
+
+/**
+ * new_dog - creating a new dog
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ * Return: pointer to the new dog (Success), NULL otherwise
+ */
+
+
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+dog_t *dog = malloc(sizeof(dog_t));
+if (dog == NULL)
+{
+printf("Unable to allocate memory for dog");
+return (NULL);
+}
+dog->name = name;
+dog->age = age;
+dog->owner = owner;
+return (dog);
+}
